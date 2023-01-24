15346a40c709c14dbee9ed464ae20918a196a920
WesleyMatthee <wrmatthee@gmail.com>
2023-01-24 13:19:11 +0000

    Add exercise 6, needs debugging

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,12 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
+Store.update_all has_many: employees
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Gary", last_name: "Yipp", hourly_rate: 55)
+@store2.employees.create(first_name: "Wesley", last_name: "Matthee", hourly_rate: 75)
+@store2.employees.create(first_name: "Eleanor", last_name: "Langley", hourly_rate: 65)
+@store2.employees.create(first_name: "Kennedy", last_name: "Ash", hourly_rate: 55)
+
+puts "Employees at #{@store2} are #{@store2.employees}."
