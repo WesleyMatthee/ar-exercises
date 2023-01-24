248325e3f1eb0443ea7d11f47c5a163c07a357d8
WesleyMatthee <wrmatthee@gmail.com>
2023-01-24 00:29:32 +0000

    Add exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,7 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+store3 = Store.find_by(id:3)
+store3.destroy
+
+puts "Store three destroyed store count should be 2: #{Store.count}."
